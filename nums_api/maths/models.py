00a7301adfcbc4d78bf2232a44f79e85bd56b170
Sara Heath <saramaheath@gmail.com>
2022-08-03 15:38:25 -0500

    added initial tests for math models and initialized math model

--- nums_api/maths/models.py
+++ nums_api/maths/models.py
@@ -1,1 +1,38 @@
 from nums_api.database import db
+
+class Math(db.Model):
+    """General math facts about numbers"""
+    __tablename__ = "math"
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+        autoincrement=True
+    )
+
+    number = db.Column(
+        db.Integer,
+        nullable=False
+    )
+
+    # fact with no prefix, first word lowercase, no punctuation at the end
+    fact_fragment = db.Column(
+        db.String(200),
+        nullable=False
+        )
+
+    # fact with prefix, first word is number, has punctuation at the end
+    fact_statement = db.Column(
+        db.String(250),
+        nullable=False
+    )
+
+    pos = db.Column(
+        db.String(20), 
+        nullable=False
+    )
+
+    self = db.Column(
+        db.Boolean,
+        nullable=False
+    )
