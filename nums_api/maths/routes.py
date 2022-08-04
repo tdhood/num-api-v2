80f316d3fb8851e63aa00a2a02e0fdb42842bec5
Sara Heath <84295202+saramaheath@users.noreply.github.com>
2022-08-04 16:53:53 -0500

    3 implement the math routes (#24)
    * fleshing out math routes
    
    * added testing for math routes
    
    *testing /rando
    *testing /1 valid number
    *testing /one invalid number
    *testing /random
    *currently tests failing and passing as expected
    
    * working through creating structure of return json
    
    * updated seed.py to include additional finished models
    
    * complete math routes and math route tests
    
    *route for api/math/number
    *route for api/math/random
    *testing for both routes
    
    Co-authored-by: Jason <jasoncmcvicar@gmail.com>

--- nums_api/maths/routes.py
+++ nums_api/maths/routes.py
@@ -1,21 +1,66 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
+from random import choice
+
+from nums_api.maths.models import Math
+
 
 math = Blueprint("math", __name__)
 
 
-@math.get("/<number>")
+@math.get("/<int:number>")
 def get_num_fact(number):
-    """FIXME
-    Stub route for getting math fact about number
+    """ Returns a random math fact in JSON about a number passed as a URL
+    parameter.
+
+    If fact is found -->
+        {fact: {number, fragment, statement, type}}
+    If no fact is found -->
+        {error: {status, message}} and a status code 404 is sent
     """
 
-    return f"Some interesting math fact about {number}."
+    math_instances = (
+        Math
+        .query
+        .filter(Math.number == number)
+        .all())
+
+    # if there are no instances then respond with below
+    if not math_instances:
+        error = {
+            'status': 404,
+            'message': f'A math fact for { number } not found',
+        }
+
+        return (jsonify(error=error), 404)
+
+    # picks a random instance from the list
+    math = choice(math_instances)
+    fact = {
+        'number': math.number,
+        'fragment': math.fact_fragment,
+        'statement': math.fact_statement,
+        'type': 'math'
+    }
+
+    return jsonify(fact=fact)
 
 
 @math.get("/random")
 def get_num_fact_random():
-    """FIXME
-    Stub route for getting math fact about random number
+    """ Returns a random math fact in JSON
+
+    Ex: {fact: {number, fragment, statement, type}}
     """
 
-    return "Some interesting math fact about a random number."
+    math_instances = Math.query.all()
+
+    # picks a random instance from the list
+    random_math = choice(math_instances)
+    fact = {
+        'number': random_math.number,
+        'fragment': random_math.fact_fragment,
+        'statement': random_math.fact_statement,
+        'type': 'math'
+    }
+
+    return jsonify(fact=fact)
--- nums_api/seed.py
+++ nums_api/seed.py
@@ -1,7 +1,8 @@
 from nums_api import app
 from nums_api.database import db
-
-# import all models - necessary for create_all()
+from nums_api.trivia.models import Trivia
+from nums_api.maths.models import Math
+from nums_api.years.models import Year
 
 db.drop_all(app=app)
 db.create_all(app=app)
