7357c2c9f03f4ade27bba82502102acbcde80c1c
LaithLou <103068838+LaithLou@users.noreply.github.com>
2022-08-09 17:50:04 -0400

    10 create the date model (#27)
    * reviewing date model
    
    * fixing test
    - changed number to day_of_year
    
    * adding helper functions
    - added day_to_day_of_year and day_to_string
    
    * minor changes
    
    * working on testing
    
    * modified logic
    - fixed bug where the output of day_to_day_of_year returns a string with leading zeros
    - fixed if logic for day_to_string
    
    * added new tests
    -  test_day_to_string
    -  test_date_to_day_of_year
    
    * preparing for PR review
    
    * working on changes requested by Spencer
    
    * added additional comments and docTests
    - added a try block
    
    * added tests for invalid input
    
    * small tweaks before merging in
    
    edited some comments in the class methods and simplified model tests a bit.
    
    Co-authored-by: Ian <ibdao921@gmail.com>
    Co-authored-by: sarmini1 <spencerarmini@gmail.com>

--- nums_api/dates/models.py
+++ nums_api/dates/models.py
@@ -1,1 +1,129 @@
 from nums_api.database import db
+import datetime
+
+
+class Date (db.Model):
+    """General date facts about numbers"""
+
+    __tablename__ = "dates"
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+        autoincrement=True
+    )
+
+    # 1 - 366 to accommodate for leap years
+    day_of_year = db.Column(
+        db.Integer,
+        nullable=False
+    )
+
+    year = db.Column(
+        db.Integer,
+        nullable=False
+    )
+
+    # fact with no prefix, first word lowercase, no punctuation at the end
+    fact_fragment = db.Column(
+        db.String(200),
+        nullable=False
+    )
+
+    # fact with prefix, first word is number, has punctuation at the end
+    fact_statement = db.Column(
+        db.String(250),
+        nullable=False
+    )
+
+    was_submitted = db.Column(
+        db.Boolean,
+        nullable=False
+    )
+
+    @classmethod
+    def date_to_day_of_year(cls, month, day):
+        """ A helper function to convert a month and day to the day of year.
+
+            - Takes month and day as integers.
+            - Returns day of year as an integer.
+
+            If month or day is out of bounds,
+                raises an ValueError with invalid message.
+
+            >>> Date.date_to_day_of_year(2, 28)
+            59
+
+            >>> Date.date_to_day_of_year(2, 29)
+            60
+
+            >>> Date.date_to_day_of_year(3, 1)
+            61
+
+            >>> Date.date_to_day_of_year(12, 31)
+            366
+        """
+
+        try:
+            # Use datetime to convert a given month and day to the day of year.
+            # Using 2004 as year, since our model expects day range to always
+            # be 1-366 (year itself doesn't matter, just needs to be a leap year).
+            # Do not change the year here-- there's no reason to.
+            # Resultant day_of_year will be a zero-padded string of integers.
+            day_of_year = datetime.datetime(2004, month, day).strftime("%j")
+
+        except ValueError:
+            raise ValueError("Invalid month or day")
+
+        # Returns an integer of the string with no leading zeros using lstrip()
+        return int(day_of_year.lstrip('0'))
+
+    @classmethod
+    def day_to_string(cls, day):
+        """ A helper function that converts a day (passed as an integer) to its
+                string equivalent using datetime library.
+
+             - Takes day of year as a integer between 1 - 366.
+             - Return date as a string like "January 1st"
+
+            >>> Date.day_to_string(1)
+            'January 1st'
+
+            >>> Date.day_to_string(59)
+            'February 28th'
+
+            >>> Date.day_to_string(3)
+            'January 3rd'
+
+            >>> Date.day_to_string(33)
+            'February 2nd'
+        """
+
+        try:
+            # Using datetime function to convert given day of year to month and
+            # day. Using 2004 as year, since our model expects day range to
+            # always be 1-366 (year itself doesn't matter, just needs to be a
+            # leap year). There should not be any reason to change the year.
+            # Resultant variable will be a string of the month and day, where
+            # the day may include one leading zero, like "January 01"
+            date_string = datetime.datetime.strptime("2004" + "-" + str(day),
+                "%Y-%j").strftime("%B %d")
+
+        except ValueError:
+            raise ValueError("Invalid day of year")
+
+        # removes leading 0 for single digit days
+        if date_string[-2] == "0":
+            date_string = date_string.replace(date_string[-2], "")
+
+        # adds number suffixes
+        if date_string[-1] == "1":
+            date_string += "st"
+        elif date_string[-1] == "2":
+            date_string += "nd"
+        elif date_string[-1] == "3":
+            date_string += "rd"
+        else:
+            date_string += "th"
+
+        return date_string
--- nums_api/dates/tests/test_models.py
+++ nums_api/dates/tests/test_models.py
@@ -2,6 +2,7 @@ from unittest import TestCase
 from nums_api import app
 from nums_api.database import db
 from nums_api.config import DATABASE_URL_TEST
+from nums_api.dates.models import Date
 
 app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL_TEST
 app.config["TESTING"] = True
@@ -15,6 +16,16 @@ class DateModelTestCase(TestCase):
     def setUp(self):
         """Set up test data here"""
 
+        Date.query.delete()
+
+        self.d1 = Date(
+            day_of_year=1,
+            year=2004,
+            fact_fragment="is the day in 2004 test date fragment",
+            fact_statement="January 1st is the day in 2004 test date statement.",
+            was_submitted=False
+        )
+
     def tearDown(self):
         """Clean up any fouled transaction."""
         db.session.rollback()
@@ -23,3 +34,29 @@ class DateModelTestCase(TestCase):
         """Test to make sure tests are set up correctly"""
         test_setup_correct = True
         self.assertEqual(test_setup_correct, True)
+
+    def test_model(self):
+        """Test ability to create new instance of Date model
+        and add instance to database successfully"""
+        self.assertIsInstance(self.d1, Date)
+        self.assertEqual(Date.query.count(), 0)
+
+        db.session.add(self.d1)
+        db.session.commit()
+
+        self.assertEqual(Date.query.count(), 1)
+        self.assertEqual(Date.query.filter_by(day_of_year=1).one().day_of_year, 1)
+
+    def test_invalid_date_to_day_of_year(self):
+        """ Test the class method to get the day of year from month and day."""
+
+        with self.assertRaises(ValueError) as exc:
+            Date.date_to_day_of_year(13,1)
+        self.assertEqual(str(exc.exception), "Invalid month or day")
+
+    def test_invalid_day_to_string(self):
+        """ Test the class method to get day of year to string equivalent."""
+
+        with self.assertRaises(ValueError) as exc:
+            Date.day_to_string(400)
+        self.assertEqual(str(exc.exception),"Invalid day of year")
