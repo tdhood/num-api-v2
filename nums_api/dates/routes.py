87b3110322c4b3051f8a5094f1be8943f273a8a3
Jordan Asano <102700387+jordanasano@users.noreply.github.com>
2022-08-11 16:58:07 -0700

    11 date route (#32)
    * Added route and tests for api/date/random
    
    * Completed route for api/date/:month/:day
    
    * Completed tests for api/date/:month/:day
    
    * Reworked our validation and try catch
    
    - Our validation and try catch now relies on the class method from Date class
    
    * Removed unnecessary test and added one for ensuring Feb 31st throws an error
    
    * couple small test tweaks
    
    Co-authored-by: Jason <jasoncmcvicar@gmail.com>
    Co-authored-by: Brittiany Juravic <brittiany1212@gmail.com>

--- nums_api/dates/routes.py
+++ nums_api/dates/routes.py
@@ -1,24 +1,85 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
 from nums_api.limiter import shared_limit
+from random import choice
+from nums_api.dates.models import Date
 
 dates = Blueprint("dates", __name__)
 
 
-@dates.get("/<month>/<day>")
+@dates.get("/<int:month>/<int:day>")
 @shared_limit
 def get_num_fact(month, day):
-    """FIXME
-    Stub route for getting fact about date
+    """ Returns a random date fact in JSON about a month/day passed as a URL
+    parameter.
+
+    If fact is found -->
+        {fact: {number, year, fragment, statement, type}}
+    If no fact is found -->
+        {error: {status, message}} and a status code 404 is sent
+    If month/day is invalid -->
+        {error: {status, message}} and a status code 400 is sent
     """
 
-    return f"Some interesting fact about the date {month}/{day}."
+    day_of_year = None
+
+    try:
+        day_of_year = Date.date_to_day_of_year(month,day)
+
+    except ValueError:
+        error = {
+            'status': 400,
+            'message': f'{ month }/{ day } is not valid date. Please give' +
+            ' valid date in URL.',
+        }
+        return (jsonify(error=error), 400)
+
+    date_instances = (
+        Date
+        .query
+        .filter(Date.day_of_year == day_of_year)
+        .all()
+    )
+
+    # if there are no instances then respond with below
+    if not date_instances:
+        error = {
+            'status': 404,
+            'message': f'A date fact for { month }/{ day } not found',
+        }
+
+        return (jsonify(error=error), 404)
+
+    # picks a random instance from the list
+    random_date = choice(date_instances)
+    fact = {
+        'number': random_date.day_of_year,
+        'year': random_date.year,
+        'fragment': random_date.fact_fragment,
+        'statement': random_date.fact_statement,
+        'type': 'date'
+    }
+
+    return jsonify(fact=fact)
 
 
 @dates.get("/random")
 @shared_limit
 def get_num_fact_random():
-    """FIXME
-    Stub route for getting fact about random date
-    """
+    """ Returns a random date fact in JSON
 
-    return "Some interesting fact about a random date."
+    Ex: {fact: {number, year, fragment, statement, type}}
+    """
+    
+    date_instances = Date.query.all()
+
+    # picks a random instance from the list
+    random_date = choice(date_instances)
+    fact = {
+        'number': random_date.day_of_year,
+        'year': random_date.year,
+        'fragment': random_date.fact_fragment,
+        'statement': random_date.fact_statement,
+        'type': 'date'
+    }
+
+    return jsonify(fact=fact)
--- nums_api/dates/tests/test_routes.py
+++ nums_api/dates/tests/test_routes.py
@@ -6,6 +6,7 @@ from nums_api.limiter import limiter
 from nums_api import app
 from nums_api.database import db
 from nums_api.config import DATABASE_URL_TEST
+from nums_api.dates.models import Date
 
 app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL_TEST
 app.config["TESTING"] = True
@@ -20,6 +21,25 @@ class DateRouteTestCase(TestCase):
         """Set up test data here"""
         self.client = app.test_client()
 
+        Date.query.delete()
+
+        self.d1 = Date(
+            day_of_year=1,
+            year=2000,
+            fact_fragment="the date for this test fragment",
+            fact_statement="January 1st in 2000 is the date fact statement.",
+            was_submitted=False)
+
+        self.d2 = Date(
+            day_of_year=2,
+            year=2000,
+            fact_fragment="the date for this test fragment",
+            fact_statement="January 2nd in 2000 is the date fact statement.",
+            was_submitted=False)
+
+        db.session.add_all([self.d1, self.d2])
+        db.session.commit()
+
         limiter.enabled = False
 
     def tearDown(self):
@@ -30,3 +50,100 @@ class DateRouteTestCase(TestCase):
         """Test to make sure tests are set up correctly"""
         test_setup_correct = True
         self.assertEqual(test_setup_correct, True)
+
+    def test_get_date_fact(self):
+        """Test for date fact for the number 1"""
+        with self.client as c:
+            resp = c.get("api/date/1/1")
+            data = resp.get_json()
+
+            self.assertEqual(
+                data,
+                {"fact": {
+                    "number": 1,
+                    "year": 2000,
+                    "fragment": "the date for this test fragment",
+                    "statement": "January 1st in 2000 is the date fact statement.",
+                    "type": "date"
+                }})
+            self.assertEqual(resp.status_code, 200)
+
+    def test_get_date_fact_random(self):
+        """Test for date fact for random route"""
+        with self.client as c:
+            d1_fact_data = {"fact": {
+                'number': 1,
+                'year': 2000,
+                'fragment': "the date for this test fragment",
+                'statement': "January 1st in 2000 is the date fact statement.",
+                'type': 'date'
+            }
+            }
+
+            d2_fact_data = {"fact": {
+                'number': 2,
+                'year': 2000,
+                'fragment': "the date for this test fragment",
+                'statement': "January 2nd in 2000 is the date fact statement.",
+                'type': 'date'
+            }
+            }
+
+            resp = c.get("api/date/random")
+            data = resp.get_json()
+
+            self.assertIn(data, [d1_fact_data, d2_fact_data])
+            self.assertEqual(resp.status_code, 200)
+
+    def test_error_for_number_with_no_fact(self):
+        """Test error for number with no fact"""
+        with self.client as c:
+            resp = c.get("api/date/3/3")
+            data = resp.get_json()
+
+            self.assertEqual(
+                data,
+                {"error": {
+                    "message": "A date fact for 3/3 not found",
+                    "status": 404
+                }})
+            self.assertEqual(resp.status_code, 404)
+
+    def test_error_for_invalid_date(self):
+        """Test date fact with an invalid month/day"""
+        with self.client as c:
+            resp = c.get("api/date/13/1")
+            data = resp.get_json()
+
+            self.assertEqual(
+                data,
+                {"error": {
+                    "message": "13/1 is not valid date. Please give valid" +
+                    " date in URL.",
+                    "status": 400
+                }})
+            self.assertEqual(resp.status_code, 400)
+
+            resp2 = c.get("api/date/1/100")
+            data2 = resp2.get_json()
+
+            self.assertEqual(
+                data2,
+                {"error": {
+                    "message": "1/100 is not valid date. Please give valid" +
+                    " date in URL.",
+                    "status": 400
+                }})
+            self.assertEqual(resp2.status_code, 400)
+
+            resp3 = c.get("api/date/2/31")
+            data3 = resp3.get_json()
+
+            self.assertEqual(
+                data3,
+                {"error": {
+                    "message": '2/31 is not valid date. Please give' +
+                    ' valid date in URL.',
+                    "status": 400
+                }})
+            self.assertEqual(resp3.status_code, 400)
