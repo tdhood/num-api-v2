bd819f5ce52ad299520c828b6a417a1b5393792c
tdhood <taylordannhood@gmail.com>
2022-08-16 14:30:27 -0700

    building popularity function

--- nums_api/helper_functions.py
+++ nums_api/helper_functions.py
@@ -1,31 +1,42 @@
 from datetime import datetime
+from nums_api.trivia.models import Trivia
 from nums_api.models_helpers import Like
+from nums_api.models_helpers import Liked_Fact
 from datetime import date
+from nums_api.database import db
 
 
+def test_function():
+        new_liked_fact = Liked_Fact(
+                trivia_fact_id=2,
+        )
 
+        db.session.add(new_liked_fact)
+        db.session.commit()
+ 
+test_function()
 # function that queries db for top 10 facts
 # time stamp only grabing week at a time . order by grab the sum of the likes
 
-CURRENT_TIME = datetime.utcnow()
-TIME_WINDOW = CURRENT_TIME.replace(day=CURRENT_TIME.day-7)
+# CURRENT_TIME = datetime.utcnow()
+# TIME_WINDOW = CURRENT_TIME.replace(day=CURRENT_TIME.day-7)
 
-fn (category,fact_id)
+# fn (category,fact_id)
 
-like = Like (
-        fact_id=fact_id,
-        fact_category=category,
-        uniqe_id=
-)
+# like = Like (
+#         fact_id=fact_id,
+#         fact_category=category,
+#         uniqe_id=
+# )
 
 
-likes = (Like
-            .query
-            .filter(Like.timestamp).in_([TIME_WINDOW,CURRENT_TIME])
-            .group_by(Like.fact_id & Like.fact_category)
-            .order_by(Like.timestamp.desc())
-            .limit(10)
-            .all())
+# likes = (Like
+#             .query
+#             .filter(Like.timestamp).in_([TIME_WINDOW,CURRENT_TIME])
+#             .group_by(Like.fact_id & Like.fact_category)
+#             .order_by(Like.timestamp.desc())
+#             .limit(10)
+#             .all())
 
 
 
