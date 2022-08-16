baad256006e5736d67526a6d4ad6945daf0f0b18
tdhood <taylordannhood@gmail.com>
2022-08-16 14:28:32 -0700

    added trivia_like model

--- nums_api/trivia/models.py
+++ nums_api/trivia/models.py
@@ -1,4 +1,5 @@
 from nums_api.database import db
+from datetime import datetime
 
 
 class Trivia(db.Model):
@@ -33,4 +34,26 @@ class Trivia(db.Model):
         db.Boolean,
         nullable=False
     )
-        
+
+
+class Trivia_Like(db.Model):
+
+    __tablename__='trivia_likes'
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+        autoincrement=True
+    )
+    
+    trivia_fact_id = db.Column(
+        db.Integer,
+        db.ForeignKey('trivia.id'),
+        default=False,
+    )
+
+    timestamp = db.Column(
+        db.DateTime,
+        nullable=False,
+        default=datetime.utcnow,
+    )
