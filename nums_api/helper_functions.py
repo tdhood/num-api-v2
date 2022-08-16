fc8a62387229ddc1fb7e1b581de1b933b68476dd
LaithLou <laithlouay@gmail.com>
2022-08-16 13:55:05 -0400

    added a new column 'id' - the column id accounts for the same facts when they are liked multiple times

--- nums_api/helper_functions.py
+++ nums_api/helper_functions.py
@@ -1,10 +1,31 @@
+from datetime import datetime
 from nums_api.models_helpers import Like
+from datetime import date
+
+
 
 # function that queries db for top 10 facts
+# time stamp only grabing week at a time . order by grab the sum of the likes
+
+CURRENT_TIME = datetime.utcnow()
+TIME_WINDOW = CURRENT_TIME.replace(day=CURRENT_TIME.day-7)
+
+fn (category,fact_id)
+
+like = Like (
+        fact_id=fact_id,
+        fact_category=category,
+        uniqe_id=
+)
+
 
 likes = (Like
             .query
-            .filter(Like.user_id.in_(following_ids))
-            .order_by(Message.timestamp.desc())
-            .limit(100)
+            .filter(Like.timestamp).in_([TIME_WINDOW,CURRENT_TIME])
+            .group_by(Like.fact_id & Like.fact_category)
+            .order_by(Like.timestamp.desc())
+            .limit(10)
             .all())
+
+
+
