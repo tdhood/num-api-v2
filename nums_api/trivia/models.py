fa5de96866a89bed231f9482502e3b4ce9e9e03c
LaithLou <laithlouay@gmail.com>
2022-08-17 14:10:54 -0400

    fixed the models

--- nums_api/trivia/models.py
+++ nums_api/trivia/models.py
@@ -1,3 +1,4 @@
+from email.policy import default
 from nums_api.database import db
 from datetime import datetime
 
@@ -35,6 +36,11 @@ class Trivia(db.Model):
         nullable=False
     )
 
+    timestamp = db.Column(
+        db.DateTime,
+        nullable=False,
+        default=datetime.utcnow,
+    )
 
 class Trivia_Like(db.Model):
 
@@ -45,7 +51,7 @@ class Trivia_Like(db.Model):
         primary_key=True,
         autoincrement=True
     )
-    
+
     trivia_fact_id = db.Column(
         db.Integer,
         db.ForeignKey('trivia.id'),
@@ -57,3 +63,26 @@ class Trivia_Like(db.Model):
         nullable=False,
         default=datetime.utcnow,
     )
+
+    # is this fact id in table
+    # check if is_trending is on
+    # add 1 to counter points
+    # grouping by the total times a fact is in the datebase, and use that in
+    # the algorithm as 'P'
+
+    # and our time is going to follow : (time now - timestamp ) = T
+
+    # after 7 days using some type of logic, we set 'on' in is_trending to 'off'
+
+
+    # score is generated each time we use the popularity algorithm, but not
+    # stored in the db. they are going to be stored in an an array inside the
+    # function, which we will use for ranking.
+
+
+    # to turn the trending to off :
+    # we are checking the last record if the fact is still
+    # trending,* we are going to find the
+
+    # *if a fact doesn't get a like in 7 days, first instance
+    # where it's on set it to off.
