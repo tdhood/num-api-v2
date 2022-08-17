365b70c7979844264f621d39bba4d6d2ee459fb8
LaithLou <laithlouay@gmail.com>
2022-08-17 19:52:47 -0400

    delete extara docstrings

--- nums_api/trivia/models.py
+++ nums_api/trivia/models.py
@@ -62,26 +62,3 @@ class Trivia_Like(db.Model):
         nullable=False,
         default=datetime.utcnow,
     )
-
-    # is this fact id in table
-    # check if is_trending is on
-    # add 1 to counter points
-    # grouping by the total times a fact is in the datebase, and use that in
-    # the algorithm as 'P'
-
-    # and our time is going to follow : (time now - timestamp ) = T
-
-    # after 7 days using some type of logic, we set 'on' in is_trending to 'off'
-
-
-    # score is generated each time we use the popularity algorithm, but not
-    # stored in the db. they are going to be stored in an an array inside the
-    # function, which we will use for ranking.
-
-
-    # to turn the trending to off :
-    # we are checking the last record if the fact is still
-    # trending,* we are going to find the
-
-    # *if a fact doesn't get a like in 7 days, first instance
-    # where it's on set it to off.
