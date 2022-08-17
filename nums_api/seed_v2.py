64b6808a7457c1acda67ce1d0e2ba3aa16e2404b
LaithLou <laithlouay@gmail.com>
2022-08-17 17:45:08 -0400

    added tests

--- nums_api/seed_v2.py
+++ nums_api/seed_v2.py
@@ -1,9 +1,11 @@
+from sqlite3 import Timestamp
 from nums_api import app
 from nums_api.database import db
 from nums_api.trivia.models import Trivia,Trivia_Like
 from nums_api.maths.models import Math,Math_Like
 from nums_api.years.models import Year,Year_Like
 from nums_api.dates.models import Date,Date_Like
+from datetime import datetime
 
 
 
@@ -14,7 +16,8 @@ trivia_fact = Trivia(
     number=2,
     fact_fragment='hello',
     fact_statement='hello_world',
-    was_submitted=True
+    was_submitted=True,
+    timestamp=datetime(2002,2,2)
 )
 
 db.session.add(trivia_fact)
@@ -24,7 +27,8 @@ trivia_fact = Trivia(
     number=3,
     fact_fragment='mello',
     fact_statement='mello_world',
-    was_submitted=True
+    was_submitted=True,
+    timestamp=datetime(2004,2,2)
 )
 
 db.session.add(trivia_fact)
@@ -34,7 +38,8 @@ trivia_fact = Trivia(
     number=4,
     fact_fragment='testing',
     fact_statement='tesssst',
-    was_submitted=True
+    was_submitted=True,
+    timestamp=datetime(2006,2,2)
 )
 
 db.session.add(trivia_fact)
@@ -44,7 +49,8 @@ year_fact = Year(
     number=3,
     fact_fragment='new',
     fact_statement='new_year',
-    was_submitted=True
+    was_submitted=True,
+    timestamp=datetime(2002,2,2)
 )
 
 db.session.add(year_fact)
@@ -54,7 +60,8 @@ year_fact = Year(
     number=1,
     fact_fragment='dew',
     fact_statement='dew_year',
-    was_submitted=True
+    was_submitted=True,
+    timestamp=datetime(2002,4,2)
 )
 
 db.session.add(year_fact)
@@ -64,7 +71,8 @@ math_fact = Math(
     number=1,
     fact_fragment='math',
     fact_statement='math_fact',
-    was_submitted=True
+    was_submitted=True,
+    timestamp=datetime(2005,2,2)
 )
 
 db.session.add(math_fact)
@@ -74,7 +82,8 @@ math_fact = Math(
     number=3,
     fact_fragment='math',
     fact_statement='More math_fact',
-    was_submitted=True
+    was_submitted=True,
+    timestamp=datetime(2012,2,2)
 )
 
 db.session.add(math_fact)
@@ -85,7 +94,8 @@ date_fact = Date(
     year=1990,
     fact_fragment='today',
     fact_statement='today nothing happened',
-    was_submitted=True
+    was_submitted=True,
+    timestamp=datetime(2018,2,2)
 )
 
 db.session.add(date_fact)
@@ -96,7 +106,8 @@ date_fact = Date(
     year=1991,
     fact_fragment='today',
     fact_statement='today nothing happened again',
-    was_submitted=True
+    was_submitted=True,
+    timestamp=datetime(2022,2,2)
 )
 db.session.add(date_fact)
 db.session.commit()
